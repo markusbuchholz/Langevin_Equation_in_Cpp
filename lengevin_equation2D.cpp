61200acae9cce2976c4145991bb38272b5a9e8b7
Markus Buchholz <30973337+markusbuchholz@users.noreply.github.com>
2022-11-15 21:40:57 +0000

    first commit

---
+++ lengevin_equation2D.cpp
@@ -0,0 +1,172 @@
+// Markus Buchholz
+
+//// g++ 3D_line_for_RRT.cpp -o t -I/usr/include/python3.8 -lpython3.8
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <math.h>
+#include <random>
+
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+
+//----------- system dynamic parameters --------------------
+
+float m = 0.05f;
+float G = 1.0f;
+float r = 1.0f;
+float dt = 0.01f;
+float R = 100.0f;
+
+float noise()
+{
+
+    std::default_random_engine generator;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<float> distribution(0.0, 1.0);
+
+    return distribution(gen);
+}
+
+//-----------------------------------------------------------
+// vx
+float function1(float x, float y, float vx, float vy)
+{
+
+    return vx;
+}
+
+//-----------------------------------------------------------
+// vy
+float function2(float x, float y, float vx, float vy)
+{
+
+    return vy;
+}
+
+//-----------------------------------------------------------
+// vx_dot
+float function3(float x, float y, float vx, float vy)
+{
+
+    float ret = -(G / m) * vx + R * noise();
+
+    return ret;
+}
+
+//-----------------------------------------------------------
+// vy_dot
+float function4(float x, float y, float vx, float vy)
+{
+
+    float ret = -(G / m) * vy + R * noise();
+    return ret;
+}
+
+//-----------------------------------------------------------
+
+std::tuple<std::vector<float>, std::vector<float>, std::vector<float>, std::vector<float>, std::vector<float>> methodRuneKuttaLangevin()
+{
+
+    std::vector<float> diffEq1;
+    std::vector<float> diffEq2;
+    std::vector<float> diffEq3;
+    std::vector<float> diffEq4;
+
+    std::vector<float> time;
+
+    // init values
+    float x1 = 0.0; //
+    float x2 = 0.0;
+    float x3 = 10.0; // 
+    float x4 = 10.0; // 
+    float t = 0.0;   // 
+
+    diffEq1.push_back(x1);
+    diffEq2.push_back(x2);
+    diffEq3.push_back(x3);
+    diffEq4.push_back(x4);
+    time.push_back(t);
+
+    for (int ii = 0; ii < 10000; ii++)
+    {
+        t = t + dt;
+        float k11 = function1(x1, x2, x3, x4);
+        float k12 = function2(x1, x2, x3, x4);
+        float k13 = function3(x1, x2, x3, x4);
+        float k14 = function4(x1, x2, x3, x4);
+
+        float k21 = function1(x1 + dt / 2 * k11, x2 + dt / 2 * k12, x3 + dt / 2 * k13, x4 + dt / 2 * k14);
+        float k22 = function2(x1 + dt / 2 * k11, x2 + dt / 2 * k12, x3 + dt / 2 * k13, x4 + dt / 2 * k14);
+        float k23 = function3(x1 + dt / 2 * k11, x2 + dt / 2 * k12, x3 + dt / 2 * k13, x4 + dt / 2 * k14);
+        float k24 = function4(x1 + dt / 2 * k11, x2 + dt / 2 * k12, x3 + dt / 2 * k13, x4 + dt / 2 * k14);
+
+        float k31 = function1(x1 + dt / 2 * k21, x2 + dt / 2 * k22, x3 + dt / 2 * k23, x4 + dt / 2 * k24);
+        float k32 = function2(x1 + dt / 2 * k21, x2 + dt / 2 * k22, x3 + dt / 2 * k23, x4 + dt / 2 * k24);
+        float k33 = function3(x1 + dt / 2 * k21, x2 + dt / 2 * k22, x3 + dt / 2 * k23, x4 + dt / 2 * k24);
+        float k34 = function4(x1 + dt / 2 * k21, x2 + dt / 2 * k22, x3 + dt / 2 * k23, x4 + dt / 2 * k24);
+
+        float k41 = function1(x1 + dt * k31, x2 + dt * k32, x3 + dt * k33, x4 + dt * k34);
+        float k42 = function2(x1 + dt * k31, x2 + dt * k32, x3 + dt * k33, x4 + dt * k34);
+        float k43 = function3(x1 + dt * k31, x2 + dt * k32, x3 + dt * k33, x4 + dt * k34);
+        float k44 = function4(x1 + dt * k31, x2 + dt * k32, x3 + dt * k33, x4 + dt * k34);
+
+        x1 = x1 + dt / 6.0 * (k11 + 2 * k21 + 2 * k31 + k41);
+        x2 = x2 + dt / 6.0 * (k12 + 2 * k22 + 2 * k32 + k42);
+        x3 = x3 + dt / 6.0 * (k13 + 2 * k23 + 2 * k33 + k43);
+        x4 = x4 + dt / 6.0 * (k14 + 2 * k24 + 2 * k34 + k44);
+
+        diffEq1.push_back(x1);
+        diffEq2.push_back(x2);
+        diffEq3.push_back(x3);
+        diffEq4.push_back(x4);
+        time.push_back(t);
+    }
+
+    return std::make_tuple(diffEq1, diffEq2, diffEq3, diffEq4, time);
+}
+
+//---------------------------------------------------------------------------------------------------------
+
+void plot2D(std::tuple<std::vector<float>, std::vector<float>> data1)
+{
+
+    std::vector<float> xX1 = std::get<0>(data1);
+    std::vector<float> yY1 = std::get<1>(data1);
+
+    plt::scatter(xX1, yY1);
+    plt::xlabel("X");
+    plt::ylabel("Y");
+    plt::show();
+}
+
+void plot2D2D(std::tuple<std::vector<float>, std::vector<float>> data1, std::tuple<std::vector<float>, std::vector<float>> data2)
+{
+
+    std::vector<float> xX1 = std::get<0>(data1);
+    std::vector<float> yY1 = std::get<1>(data1);
+
+    std::vector<float> xX2 = std::get<0>(data2);
+    std::vector<float> yY2 = std::get<1>(data2);
+
+    plt::plot(xX1, yY1);
+    plt::plot(xX2, yY2);
+    plt::xlabel("X[m]");
+    plt::ylabel("Y[m]");
+    plt::show();
+}
+
+//---------------------------------------------------------------
+int main()
+{
+
+    std::tuple<std::vector<float>, std::vector<float>, std::vector<float>, std::vector<float>, std::vector<float>> particle = methodRuneKuttaLangevin();
+
+    std::tuple<std::vector<float>, std::vector<float>> XY = std::make_tuple(std::get<0>(particle), std::get<1>(particle));
+    std::tuple<std::vector<float>, std::vector<float>> vx = std::make_tuple(std::get<4>(particle), std::get<2>(particle));
+
+    plot2D(XY);
+    plot2D(vx);
+}
